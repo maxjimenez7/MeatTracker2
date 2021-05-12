f66f5348188bd18cc14bfc6505af0fa83e89581c
per4 <mrjimenez.410413@gmail.com>
2021-05-12 11:14:29 -0700

    Includes button_main_calculate onClickListener Has input_consumption.xml format started (chicken and beef only)

---
+++ app/src/main/java/com/example/meattracker/Calculate.kt
@@ -0,0 +1,14 @@
+package com.example.meattracker
+
+object Calculate {
+
+    val water: Int = 0
+    val grains: Int = 0
+    val topSoil: Int = 0
+
+    //fun calculateWater()
+    // params chicken beef pork
+    // return these values
+    // formulas class
+
+}
--- app/src/main/java/com/example/meattracker/MainActivity.kt
+++ app/src/main/java/com/example/meattracker/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.meattracker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.input_consumption.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,16 +19,27 @@ class MainActivity : AppCompatActivity() {
         button_main_calculate.setOnClickListener {
             //button_main_calculate.visibility = View.GONE
             //textView_main_title.visibility = View.GONE
-            setContentView(R.layout.input_consumption)
-            Toast.makeText(this@MainActivity, "Input consumption now", Toast.LENGTH_LONG)
-        }
+            // intent instead to input consump xml
+            // add listener to edit text fields called textwatcher
+            val intent = Intent(this, R.layout.input_consumption::class.java).apply {
+                Toast.makeText(this@MainActivity, "Input consumption now", Toast.LENGTH_LONG)
 
+            }
+            startActivity(intent)
+
+        }
     }
 
     fun inputConsumption(){
-        //editTextNumber_main_input_beef_consumption.setOnEdit
-        }
-        
+        var beef : Int = editTextNumber_input_consumption_beef.text.toString().toInt()
+        //beef = String.
+        val beefConsumption = beef * 2437.5
+    }
+
+
 }
+
+
+
 
 
